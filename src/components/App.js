7d66c03f8feea0ad7e620c8bb26c04889fe75a43
Linh Le Tuan <linh.tuan@actum.cz>
2016-12-25 16:13:52 +0100

    video 19 - Persisting Order State with localstorage

--- src/components/App.js
+++ src/components/App.js
@@ -1012,6 +1012,187 @@
 // ===========================================================================
 // #18video-03: using componentWillUnmount()
 
+// import React from 'react';
+
+// import Header from './Header';
+// import Order from './Order';
+// import Inventory from './Inventory';
+// import sampleFishes from '../sample-fishes';
+// import Fish from './Fish';
+// import base from '../base';
+
+// class App extends React.Component {
+// 	constructor() {
+// 		super();
+// 		this.addFish = this.addFish.bind(this);
+// 		this.loadSamples = this.loadSamples.bind(this);
+// 		this.addToOrder = this.addToOrder.bind(this);
+
+// 		//getInitial state
+// 		this.state = {
+// 			fishes: {},
+// 			order: {}
+// 		};
+// 	}
+
+// 	componentWillMount() {
+// 		this.ref = base.syncState(`${this.props.params.storeId}/fishes`, 
+// 		{
+// 			context: this,
+// 			state: 'fishes'
+// 		});
+// 	}
+
+// 	componentWillUnmount(){
+// 		base.removeBinding(this.ref);
+// 	}
+
+// 	addFish(fish) {
+// 		// update our state
+// 		const fishes = {...this.state.fishes}; // take the current state of fishes and spread it = make a copy of it :D
+// 		// add in our new fish
+// 		const timestamp = Date.now();
+// 		fishes[`fish-${timestamp}`] = fish;
+// 		//set state
+// 		// this.setState( {fishes: fishes} );
+// 		// or:
+// 		this.setState({fishes});
+// 	}
+
+// 	loadSamples() {
+// 		this.setState({
+// 			fishes: sampleFishes
+// 		})
+// 	}
+
+// 	addToOrder(key) {
+// 		// copy the current state of order
+// 		const order = {...this.state.order};
+// 		// update or add new number of fish to order
+// 		order[key] = order[key] + 1 || 1;
+// 		// update our state of order
+// 		this.setState({ order });
+// 	}
+
+// 	render() {
+// 		return (
+// 			<div className="catch-of-the-day">
+// 				<div className="menu">
+// 					<Header tagline="Fresh Seafood Market" />
+// 					<ul className="list-of-fishes">
+// 						{
+// 							Object.keys(this.state.fishes).map(key => <Fish key={key} index={key}details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)
+// 						}
+// 					</ul>
+// 				</div>
+
+// 				<Order fishes={this.state.fishes} order={this.state.order}/>
+// 				<Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
+// 			</div>
+// 		)
+// 	}
+// }
+
+// export default App;
+// constinue with video #19: persisting order state with localstorage
+
+// ===========================================================================
+// #19video-01: add new method componentWillUpdate, and pass all params to order, in localStorage you can not store object, only string is allowed = use JSON.stringify()
+
+// import React from 'react';
+
+// import Header from './Header';
+// import Order from './Order';
+// import Inventory from './Inventory';
+// import sampleFishes from '../sample-fishes';
+// import Fish from './Fish';
+// import base from '../base';
+
+// class App extends React.Component {
+// 	constructor() {
+// 		super();
+// 		this.addFish = this.addFish.bind(this);
+// 		this.loadSamples = this.loadSamples.bind(this);
+// 		this.addToOrder = this.addToOrder.bind(this);
+
+// 		//getInitial state
+// 		this.state = {
+// 			fishes: {},
+// 			order: {}
+// 		};
+// 	}
+
+// 	componentWillMount() {
+// 		this.ref = base.syncState(`${this.props.params.storeId}/fishes`, 
+// 		{
+// 			context: this,
+// 			state: 'fishes'
+// 		});
+// 	}
+
+// 	componentWillUnmount(){
+// 		base.removeBinding(this.ref);
+// 	}
+
+// 	componentWillUpdate(nextProps, nextState) {
+// 		localStorage.setItem(`order-${this.props.params.storeId}`, JSON.stringify(nextState.order))
+// 	}
+
+// 	addFish(fish) {
+// 		// update our state
+// 		const fishes = {...this.state.fishes}; // take the current state of fishes and spread it = make a copy of it :D
+// 		// add in our new fish
+// 		const timestamp = Date.now();
+// 		fishes[`fish-${timestamp}`] = fish;
+// 		//set state
+// 		// this.setState( {fishes: fishes} );
+// 		// or:
+// 		this.setState({fishes});
+// 	}
+
+// 	loadSamples() {
+// 		this.setState({
+// 			fishes: sampleFishes
+// 		})
+// 	}
+
+// 	addToOrder(key) {
+// 		// copy the current state of order
+// 		const order = {...this.state.order};
+// 		// update or add new number of fish to order
+// 		order[key] = order[key] + 1 || 1;
+// 		// update our state of order
+// 		this.setState({ order });
+// 	}
+
+// 	render() {
+// 		return (
+// 			<div className="catch-of-the-day">
+// 				<div className="menu">
+// 					<Header tagline="Fresh Seafood Market" />
+// 					<ul className="list-of-fishes">
+// 						{
+// 							Object.keys(this.state.fishes).map(key => <Fish key={key} index={key}details={this.state.fishes[key]} addToOrder={this.addToOrder}/>)
+// 						}
+// 					</ul>
+// 				</div>
+
+// 				<Order
+// 					fishes={this.state.fishes}
+// 					order={this.state.order}
+// 					params={this.props.params}
+// 				/>
+// 				<Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
+// 			</div>
+// 		)
+// 	}
+// }
+
+// export default App;
+
+// ===========================================================================
+// #19video-02: check if any order is in localStorages in componentWillMount
+
 import React from 'react';
 
 import Header from './Header';
@@ -1036,15 +1217,28 @@ class App extends React.Component {
 	}
 
 	componentWillMount() {
-		this.ref = base.syncState(`${this.props.params.storeId}/fishes`, 
+		// this run right before App.js is rendered
+		this.ref = base.syncState(`${this.props.params.storeId}/fishes`,
 		{
 			context: this,
 			state: 'fishes'
 		});
+		// check if there is any order in localStorage:
+		const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`);
+		if(localStorageRef) {
+			// udpate our App.js component's order state
+			this.setState({
+				order: JSON.parse(localStorageRef)
+			})
+		}
 	}
 
 	componentWillUnmount(){
 		base.removeBinding(this.ref);
+	}
+
+	componentWillUpdate(nextProps, nextState) {
+		localStorage.setItem(`order-${this.props.params.storeId}`, JSON.stringify(nextState.order))
 	}
 
 	addFish(fish) {
@@ -1086,7 +1280,11 @@ class App extends React.Component {
 					</ul>
 				</div>
 
-				<Order fishes={this.state.fishes} order={this.state.order}/>
+				<Order
+					fishes={this.state.fishes}
+					order={this.state.order}
+					params={this.props.params}
+				/>
 				<Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
 			</div>
 		)
@@ -1094,3 +1292,4 @@ class App extends React.Component {
 }
 
 export default App;
+// constinue with video #19
